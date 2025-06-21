12f6bb9db0f2766235fc57f43dc52766bb3a4ea8
nice2ceeu <123889451+nice2ceeu@users.noreply.github.com>
2025-06-22 01:49:33 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -13,21 +13,3 @@ if (!$conn) {
 }
 
 
-// $serverName = "tcp:krissslazarte.database.windows.net,1433";
-// $connectionInfo = array(
-//     "UID" => "CloudSA38c8fe9d",
-//     "PWD" => "B09120146995b",  // <-- Must NOT be empty!
-//     "Database" => "ridb",
-//     "LoginTimeout" => 30,
-//     "Encrypt" => 1,
-//     "TrustServerCertificate" => 0
-// );
-
-// $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-// if ($conn) {
-//     echo "✅ Connection successful.";
-// } else {
-//     echo "❌ Connection failed.<br>";
-//     die(print_r(sqlsrv_errors(), true));
-// }
