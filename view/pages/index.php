33c94d5e17b3a11d1a3700efbd3a4f84e723bdf6
kristoffer <krissslazarte@gmail.com>
2025-07-13 18:59:39 +0800

    hahah

--- view/pages/index.php
+++ view/pages/index.php
@@ -35,7 +35,7 @@ if (isset($_SESSION['username'])) {
 <body class="flex items-center justify-center w-lvw h-dvh">
   <form
     method="POST"
-    action=""
+    action="/controller/login.php"
 
     class="z-10 gap-5 p-6.5 flex flex-col justify-center  shadow-[5px_5px_10px_rgba(0,0,0,0.1)] rounded-lg items-center">
     <section>
@@ -81,64 +81,3 @@ if (isset($_SESSION['username'])) {
 <script src="../script/login.js"></script>
 
 </html>
-
-
-
-<?php
-
-include_once(__DIR__ . '../../config/database.php'); 
-
-if (isset($_POST["submit"])) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    try {
-        $sql = "SELECT * FROM admin WHERE username = ?";
-        $params = array($username);
-        $stmt = sqlsrv_prepare($conn, $sql, $params);
-
-        if (!$stmt) {
-            throw new Exception(print_r(sqlsrv_errors(), true));
-        }
-
-        if (sqlsrv_execute($stmt)) {
-            $user = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-
-            if ($user) {
-                if (password_verify($password, $user['password'])) {
-                    session_start();
-                    $_SESSION['id'] = $user['id'];
-                    $_SESSION['username'] = $user['username'];
-                    $_SESSION['firstname'] = $user['firstname'];
-                    $_SESSION['lastname'] = $user['lastname'];
-                    $_SESSION['user_role'] = $user['user_role'];
-
-                    if ($_SESSION['user_role'] === "admin") {
-                        header("Location: /view/pages/Clinic-Patient.php");
-                        exit();
-                    } elseif ($_SESSION['user_role'] === "student") {
-                        header("Location: /view/pages/userprofile.php");
-                        exit();
-                    }
-                } else {
-                    session_start();
-                    $_SESSION['modal_title'] = 'Alert';
-                    $_SESSION['modal_message'] = 'Invalid Password';
-                    header("Location: /view/pages/index.php");
-                    exit();
-                }
-            } else {
-                session_start();
-                $_SESSION['modal_title'] = 'Alert';
-                $_SESSION['modal_message'] = 'User not found';
-                header("Location: /view/pages/index.php");
-                exit();
-            }
-        } else {
-            throw new Exception(print_r(sqlsrv_errors(), true));
-        }
-    } catch (Exception $e) {
-        echo "Error: " . $e->getMessage();
-    }
-}
-?>
