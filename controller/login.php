b9e1131743ab146bcb32b2d62b00743567c59698
kristoffer <krissslazarte@gmail.com>
2025-07-13 20:10:07 +0800

    gaga

--- controller/login.php
+++ controller/login.php
@@ -1,7 +1,7 @@
 <?php
 echo "Login script is being accessed.";
 echo __DIR__;
-include(__DIR__ . "/config/database.php"); 
+include(__DIR__ . '/../config/database.php');
 if (isset($_POST["submit"])) {
     $username = $_POST["username"];
     $password = $_POST["password"];
