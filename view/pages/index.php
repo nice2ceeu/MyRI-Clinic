82bb2b27477900a4832eaffb69a30523ffa5472b
nice2ceeu <123889451+nice2ceeu@users.noreply.github.com>
2025-07-14 13:46:05 +0800

    Update index.php

--- view/pages/index.php
+++ view/pages/index.php
@@ -37,7 +37,7 @@ include('/config/database.php');
 
 <body class="flex items-center justify-center w-lvw h-dvh">
   <form
-    action="/controller/login.php"
+    action="../../controller/login.php"
     method="POST"
     class="z-10 gap-5 p-6.5 flex flex-col justify-center shadow-[5px_5px_10px_rgba(0,0,0,0.1)] rounded-lg items-center">
     <section>
