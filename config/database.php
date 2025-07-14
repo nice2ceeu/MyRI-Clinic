6c4f57597aa04315e5600ec914f06f3b84309e54
nice2ceeu <123889451+nice2ceeu@users.noreply.github.com>
2025-07-14 18:13:32 +0800

    Update database.php try

--- config/database.php
+++ config/database.php
@@ -13,25 +13,25 @@
 // }
 
 
-// require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// // Load .env file
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-// $dotenv->load();
+// Load .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 // Retrieve .env values and cast to string
-// $name   = (string) $_ENV['DB_SERVER'];
-// $user   = (string) $_ENV['DB_UID'];
-// $pass   = (string) $_ENV['DB_PWD'];
-// $dbname = (string) $_ENV['DB_NAME'];
+$name   = (string) $_ENV['DB_SERVER'];
+$user   = (string) $_ENV['DB_UID'];
+$pass   = (string) $_ENV['DB_PWD'];
+$dbname = (string) $_ENV['DB_NAME'];
 
 
 
-$serverName = "tcp:krissslazarte.database.windows.net,1433";
+$serverName = $name;
 $connectionInfo = array(
-    "UID" => "CloudSA38c8fe9d",
-    "PWD" => "B09120146995b",
-    "Database" => "ridb",
+    "UID" => $user,
+    "PWD" => $pass,
+    "Database" => $dbname,
     "LoginTimeout" => 30,
     "Encrypt" => 1,
     "TrustServerCertificate" => 0
