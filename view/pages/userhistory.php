993bd65ec48a4c23d14c81025a4bf62951aec1c2
kristoffer <krissslazarte@gmail.com>
2025-07-14 15:49:11 +0800

    gag

--- view/pages/userhistory.php
+++ view/pages/userhistory.php
@@ -1,11 +1,7 @@
-<?php
 
-include('../components/body.php');
-
-?>
 <?php
 session_start();
-
+include('../components/body.php');
 if (!isset($_SESSION['username'])) {
     echo "<script>window.location.href = 'index.php';</script>";
     exit();
